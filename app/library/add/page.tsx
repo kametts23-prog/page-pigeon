ef98ff5445a778c6509b2b40db4ef3537f952dcb
kametts23-prog <kametts23@gmail.com>
2026-09-22 14:30:33 -0400

    Add app/library/add/page.tsx

---
+++ app/library/add/page.tsx
@@ -0,0 +1,9 @@
+export default function AddBook() {
+  return (
+    <main className="hero">
+      <div className="stamp">MY LIBRARY</div>
+      <h1>Send a book on its way.</h1>
+      <p className="lede">ISBN scanning and book metadata lookup will be connected in the library milestone.</p>
+    </main>
+  );
+}
